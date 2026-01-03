9afacc264ae9a0bb882bdf54d5321a6ca3d81319
Sanjukta222 <sanjuktasasmal2002@gmail.com>
2026-01-04 00:04:59 +0530

    Add importExport

---
+++ importExport/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css';
+// i am doing  import the home component ,then i used this home component inside App compo
+import Home from './Home';
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>this is my app component...</h1> 
+
+      <Home/>
+      
+       
+    </>
+  )
+}
+
+export default App
---
+++ importExport/src/Home.jsx
@@ -0,0 +1,15 @@
+function Home(){
+  return(
+    <>
+
+    <h2>it is another component.
+        this is home component.
+    </h2>
+   
+    
+    </>
+  )
+}
+// i am doing to export this home compo, because this home component i want to use another component
+
+export default Home;
