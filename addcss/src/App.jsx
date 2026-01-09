b3e38d1621442d23f7871392b3a28fc86a2c76e4
Sanjukta222 <sanjuktasasmal2002@gmail.com>
2026-01-09 20:27:55 +0530

    Add CSS

---
+++ addcss/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Home from "./components/Home";
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>app component</h1>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
---
+++ addcss/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import "./Home.css";
+
+function Home(){
+    return(
+        <div>
+            <h1 className="homo">HOME COMPONENT COMMING..</h1>
+        </div>
+    )
+}
+export default Home;
